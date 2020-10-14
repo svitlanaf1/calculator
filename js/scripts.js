27be96cd0c6510f83ab48f088e65505760bfe794
Svitlana Filatova <svitlana.filatova+1@gmail.com>
2020-10-14 13:26:57 -0700

    Add functions for a simple calculator

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+function add(number1, number2) {
+  return number1+number2;
+}
+
+function subtract(number1, number2) {
+  return number1-number2;
+}
+
+function multiply(number1, number2) {
+  return number1*number2;
+}
+
+function divide(number1, number2) {
+  return number1/number2;
+}
+
+function convertFromCToF(number){
+  return ((number * 9/5) + 32);
+}
+
+const number1 = parseInt(prompt("Enter a number"));
+const number2 = parseInt(prompt("Enter another number"));
+const result = add(number1, number2)
+alert(result);
